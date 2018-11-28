642329ad30c1263e902d4c13002a29581a20ce6d
melliti <ramzi.melliti@epitech.eu>
2018-11-29 00:37:34 +0100

    [ADD] Disable/ enable sound

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var colors = ["#B2B2C6", "#4E4E6C", "#6C4E4E", "#4E584E", "#B79EB1"];
+var enable = true;
 
 function keypressed(e) {
     keycode = document.getElementById("keycode");
@@ -6,5 +7,14 @@ function keypressed(e) {
 
     keycode.innerHTML = e.charCode + " - " + String.fromCharCode(e.charCode);
     keycode.style.color = colors[Math.floor(Math.random() * colors.length)];
-    audio.play();
+    console.log(keycode);
+    if (enable)
+        audio.play();
+}
+
+function switchSpeaker() {
+    image = document.getElementsByClassName("speaker");
+
+    enable ? enable = false : enable = true;
+    enable ? image[0].src = "../icon/speaker.png" : image[0].src = "../icon/disable_speaker.png"; 
 }
